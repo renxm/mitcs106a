2665ab92637e8e852cb5e2dc200f35a5a207a7b7
renxm <iamrzh@hotmail.com>
2014-03-20 20:53:51 +0800

    initial files of assignment 2

---
+++ Assignment2/FixingBrokenJava.java
@@ -0,0 +1,15 @@
+public class FixingBrokenJava {
+	/* Because this program contains numerous bugs that cause compiler errors,
+	 * we've chosen not to include the source code to this program directly in
+	 * the project.  That would cause all sorts of error messages when you were
+	 * working on the other parts of the assignment.
+	 * 
+	 * To get the starter code for this assignment, go to
+	 * 
+	 *    http://cs106a.stanford.edu/assignments/FixingBrokenJava.txt
+	 * 
+	 * Then, replace the contents of this file with the one from your web browser.
+	 * 
+	 * Happy debugging!
+	 */
+}
