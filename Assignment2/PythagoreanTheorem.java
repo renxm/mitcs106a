74b30de5b19c15c17fd4817bf60756067c721101
renxm <iamrzh@hotmail.com>
2014-04-02 21:19:49 +0800

    Pythagorean Theorem

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -9,7 +9,12 @@
 import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-	}
+  public void run() {
+    /* You fill this in */
+    println("Enter values to compute the Pythagorean theorem.");
+    double a = readDouble("a: ");
+    double b = readDouble("b: ");
+    double c = Math.sqrt(a*a + b*b); 
+    println("c = " + c);
+  }
 }
