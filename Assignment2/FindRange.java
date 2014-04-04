27b3bdf14b2882e346fe8af082fbcc527c2e5fe5
renxm <iamrzh@hotmail.com>
2014-04-04 16:12:20 +0800

    FindRange and Hailstone completed

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -6,11 +6,31 @@
  * This file is the starter file for the FindRange problem.
  */
 
+import java.util.ArrayList;
+
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-	}
+  private static final int SENTINEL = 0;
+  public void run() {
+    
+    int max = SENTINEL, min = SENTINEL;    
+    println("This program finds the largest and smallest numbers.");    
+    /* Read values from the user until they enter SENTINEL. */
+    while (true) {
+      int current = readInt("? ");
+      if (current == SENTINEL)
+        break;
+      if(current<min || max == SENTINEL)
+        min = current;
+      if(current>max || max == SENTINEL)
+        max = current;
+    }
+    if (max == SENTINEL || min == SENTINEL)
+      println("There are no numbers before the SENTINEL.");
+    else {
+      println("smallest: " + min);
+      println("largest: " + max);
+    }
+  }
 }
-
--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -9,8 +9,53 @@
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-	}
-}
+  public void run() {
+    /* You fill this in */
+    int initValue = readInt("Enter a number: ");
+    //collatzIterate(initValue);
+    collatzRecurse(initValue);
+  }
 
+  private int recurseSum = 0;
+  private void collatzRecurse(int n) {
+    if(n<=1) {
+      println("The process took " + recurseSum + " to reach 1");
+      return;
+    }
+    else if(isOdd(n)) {
+      print(n + " is odd, so I make 3n + 1: ");
+      n = 3*n + 1;
+      println(n);
+    } else {
+      print(n + " is even, so I take half: ");
+      n /= 2;
+      println(n);
+    }
+    recurseSum++;
+    collatzRecurse(n);
+  }
+
+  private void collatzIterate(int n) {
+    int loopSum = 0;
+    while(n>1){
+      if(isOdd(n)) {
+        print(n + " is odd, so I make 3n + 1: ");
+        n = 3*n + 1;
+        println(n);
+      } else {
+        print(n + " is even, so I take half: ");
+        n /= 2;
+        println(n);
+      }
+      loopSum ++;
+    }
+    println("The process took " + loopSum + " to reach 1");
+  }
+
+  private boolean isOdd(int n) {
+    if(n%2 == 1)
+      return true;
+    else
+      return false;
+  }
+}
